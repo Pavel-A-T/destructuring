bbe3f8b41b0e91b3eb69c8fad30ead0df4a70bb9
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-10-29 19:58:28 +0300

    two comit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 export default function dest(obj) {
-    let {name, type, health, level, attack, defence, special} = obj;
-    let arr = [];
-    for (let i = 0; i < special.length; i++) {
-        let {id, name, icon, description = 'Описание недоступно'} = special[i];
-        arr.push({id, name, icon, description});
-    }
-    return arr;
+  const { special } = obj;
+  const arr = [];
+  for (let i = 0; i < special.length; i += 1) {
+    const {
+      id, name, icon, description = 'Описание недоступно',
+    } = special[i];
+    arr.push({
+      id, name, icon, description,
+    });
+  }
+  return arr;
 }
